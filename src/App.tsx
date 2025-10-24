002981fc0934660d8c13e30011ba5e0c3c40decd
roshh-a <roshini.ananth@taffinc.com>
2025-10-24 16:36:54 +0530

    added navigation and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import './App.css';
-import { SampleForm } from './Pages/SampleForm';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Dashboard } from './Pages/Dashboard';
+import React from "react";
+import "./App.css";
+import { SampleForm } from "./Pages/SampleForm";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Dashboard } from "./Pages/Dashboard";
 
 const App: React.FC = () => {
   return (
     // must be wrapped with the BrowserRouter component to enable routing
     <BrowserRouter>
       <Routes>
-        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
 
-        <Route path='' element={<SampleForm />} />
+        <Route path="" element={<SampleForm />} />
       </Routes>
     </BrowserRouter>
   );
--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,15 +1,17 @@
 // This allows SVG can be imported as react component
-declare module '*.svg' {
-    import * as React from 'react';
-    export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>;
-    const src: string;
-    export default src;
+declare module "*.svg" {
+  import * as React from "react";
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+  const src: string;
+  export default src;
 }
 // To allow immporting all types of images in our file
-declare module '*.png';
-declare module '*.jpg';
-declare module '*.jpeg';
-declare module '*.gif';
-declare module '*.bmp';
-declare module '*.tiff';
-declare module '*.webp';
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.bmp";
+declare module "*.tiff";
+declare module "*.webp";
